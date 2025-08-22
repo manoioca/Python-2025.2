2fd14ff1648de961a229121bb206e8fa44dbdb2a
manoioca <caio93martins@gmail.com>
2025-08-21 21:44:37 -0300

    21

---
+++ strings.py
@@ -0,0 +1,42 @@
+# strings
+variavel_string = "python"
+variavel_string2 = "uma linguagem 'facil'"
+
+
+print(variavel_string = "python"):
+print(variavel_string2)
+
+print(len(variavel_string))
+print(len(variavel_string2))
+
+print(variavel_string2[2])
+print(variavel_string[2:7])
+
+print("x" * 20)
+
+print(type(variavel_string))
+
+print("SPLIT")
+print(variavel_string2.split())
+
+print("DIR")
+print(dir(variavel_string2))
+
+print("LISTA")
+outra_lista = ["casa", 5, "e", [4, 5, 6]]
+print(outra_lista)
+print(outra_lista[1])
+print(outra_lista[3][2])
+print("casa", "azul")
+print(outra_lista[1], outra_lista[3][2])
+
+print("POP")
+exemplo = [1, 6, 9, 10]
+
+print(exemplo)
+print(exemplo.pop())
+print(exemplo)
+
+ordem = ["d", "a", "c", "z"]
+print(ordem)
+print(ordem.sort())
